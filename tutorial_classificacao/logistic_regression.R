9abe7e9a7bdb79a810067b54a9cafaa971ad2f34
Tarciso Braz <tarcisocomp@gmail.com>
2017-02-06 09:26:05 -0300

    Removendo linha com erro.

--- tutorial_classificacao/logistic_regression.R
+++ tutorial_classificacao/logistic_regression.R
@@ -72,6 +72,7 @@ auc
 #Caret
 library(caret)
 data(GermanCredit)
+str(GermanCredit)
 Train <- createDataPartition(GermanCredit$Class, p=0.6, list=FALSE)
 training <- GermanCredit[ Train, ]
 testing <- GermanCredit[ -Train, ]
@@ -79,11 +80,9 @@ testing <- GermanCredit[ -Train, ]
 caret_model <- train(Class ~ Age + ForeignWorker + Property.RealEstate + Housing.Own + 
                    CreditHistory.Critical,  data=training, method="glm", family="binomial")
 
-predict(mod_fit, newdata=testing, type="prob")
+caret.probs <- predict(caret_model, newdata=testing, type="prob")
 
 caret.results <- predict(caret_model,newdata=testing)
-caret.results <- ifelse(caret.results > 0.5,1,0)
-
 confusionMatrix(data=caret.results, reference=testing$Class)
 
 accuracy <- table(caret.results, testing[,"Class"])
